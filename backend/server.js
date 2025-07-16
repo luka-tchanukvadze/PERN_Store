ae92273d025b2ed6a0f45c634e4fca2b24fb606e
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-07-16 19:42:39 +0400

    Project Setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,24 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(helmet()); // helmet is a security middleware that helps you protect your app by setting various HTTP headers
+app.use(morgan("dev")); // log the requests
+
+app.get("/", (req, res) => {
+  res.send("hello from backend");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
