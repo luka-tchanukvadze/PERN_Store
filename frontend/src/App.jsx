1a7d4778477cf5e677452486538352edbff19df2
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-07-18 23:03:54 +0400

    React / Routes Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+
 function App() {
   return (
     <>
-      <div className="">hey</div>
+      <div className="min-h-screen bg-base-200 transition-colors duration-300">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+        </Routes>
+      </div>
     </>
   );
 }
