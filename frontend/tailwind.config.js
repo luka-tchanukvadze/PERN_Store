0b341a84a6927d390e67afd1b71f496e9036345b
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-07-18 22:54:02 +0400

    init

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,4 +7,21 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "pastel",
+      "retro",
+      "coffee",
+      "forest",
+      "cyberpunk",
+      "synthwave",
+      "luxury",
+      "autumn",
+      "valentine",
+      "aqua",
+      "business",
+      "night",
+      "dracula",
+    ],
+  },
 };
