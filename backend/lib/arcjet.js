b309161dc26648dc4359d09d2dc6db755a3ad34c
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-07-18 21:11:46 +0400

    Rate Limiting & Bot Detection

---
+++ backend/lib/arcjet.js
@@ -0,0 +1,31 @@
+import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// init arcjet
+export const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  characteristics: ["ip.src"],
+  rules: [
+    //shield protectt your app from common attacks e.g. SQL injection, XSS, CSRF attacks
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE  ",
+      // block all bots except search engines
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+        // see the full list at https://arcjet.com/bot-list
+      ],
+    }),
+
+    // rate limiting
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5,
+      interval: 10,
+      capacity: 10,
+    }),
+  ],
+});
--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,43 @@ app.use(cors());
 app.use(helmet()); // helmet is a security middleware that helps you protect your app by setting various HTTP headers
 app.use(morgan("dev")); // log the requests
 
+// apply arcjet rate-limit to all routes
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, //specifies that each requesst consumes 1 token
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({
+          error: "Too many requests",
+        });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+
+      return;
+    }
+
+    // check for spoofed bots
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("arcjet", error);
+  }
+});
+
 app.use("/api/products", productRotes);
 
 async function initDB() {
