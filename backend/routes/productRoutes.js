6b3597235ba874da6dd9fac3bf5220c2116761b2
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-07-18 20:46:54 +0400

    Writing Controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,14 @@ import express from "express";
 import {
   getAllProducts,
   createProduct,
+  getProduct,
+  updateProduct,
+  deleteProduct,
 } from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get("/", getAllProducts);
-router.get("/", createProduct);
+router.route("/").get(getAllProducts).post(createProduct);
+router.route("/:id").get(getProduct).put(updateProduct).delete(deleteProduct);
 
 export default router;
