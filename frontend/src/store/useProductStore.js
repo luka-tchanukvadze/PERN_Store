6544071bc6f238565d5de0110ec89299f0be81d0
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-07-21 12:28:12 +0400

    Delete Product

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const BASE_URL = "http://localhost:3000";
 
@@ -32,9 +33,12 @@ export const useProductStore = create((set, get) => ({
       set((prev) => ({
         products: prev.products.filter((product) => product.id !== id),
       }));
+      toast.success("Product deleted successfully");
     } catch (error) {
       console.log("Error in deleteProduct function", error);
-      toast;
+      toast.error("Something went wrong");
+    } finally {
+      set({ loading: false });
     }
   },
 }));
